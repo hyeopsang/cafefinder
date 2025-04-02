47140652810447f6d6a969b0a5ef6a92634fe977
hyeopsang <dbtkdguq3@gmail.com>
2025-04-02 16:13:04 +0900

    fix: 하드코딩된 URL 수정

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const REST_API_KEY = import.meta.env.VITE_REST_API_KEY;
 export const CLIENT_SECRET = import.meta.env.VITE_KAKAO_SECRET_KEY;
 export const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI;
-export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=https://cafefinder.vercel.app/auth/kakao/callback&response_type=code`;
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
