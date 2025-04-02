e85f09f43182111e689bf292a3643e5f3f95202d
hyeopsang <dbtkdguq3@gmail.com>
2025-04-02 16:05:23 +0900

    fix: config private키 삭제

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const REST_API_KEY = import.meta.env.VITE_REST_API_KEY;
 export const CLIENT_SECRET = import.meta.env.VITE_KAKAO_SECRET_KEY;
 export const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI;
-export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=d253ba4966bb63fb939927edcde1d6ae&redirect_uri=https://cafefinder.vercel.app/auth/kakao/callback&response_type=code`;
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=https://cafefinder.vercel.app/auth/kakao/callback&response_type=code`;
