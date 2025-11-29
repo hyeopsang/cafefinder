3c7839a170692df78ef73064d136d09927a93bff
hyeopsang <dbtkdguq3@gmail.com>
2025-11-30 01:43:47 +0900

    refactor(home): 헤더 높이 변경

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,7 +2,7 @@ import Logo from '@/assets/logo.svg?react';
 
 export default function Header() {
   return (
-    <header className="flex h-18 w-full items-center overflow-hidden px-1 py-3">
+    <header className="flex h-14 w-full items-center overflow-hidden px-1 py-3">
       <Logo className="h-20 w-40" />
     </header>
   );
